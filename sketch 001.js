287dc52d88748219aa2e17bdaf51d5760e2772e9
Yuanqing <yl11249@nyu.edu>
2023-09-23 18:02:24 -0400

    Dots

---
+++ sketch 001.js
@@ -0,0 +1,21 @@
+let maxDiam = 100
+let minDiam = maxDiam / 10;
+
+function setup() {
+    createCanvas(600, 600);
+}
+
+
+function draw() {
+    background("orange");
+    fill("pink");
+    randomSeed(0)
+
+    for (let xpos = 0; xpos <= width; xpos += 2 * maxDiam) {
+       for (let ypos = 0; ypos <= height; ypos += 2 * maxDiam) {
+           let tDiam = random(minDiam, maxDiam);
+           ellipse (xpos, ypos, tDiam, tDiam);
+        }
+     }
+}
+
--- sketch.js
+++ sketch.js
@@ -1,5 +1,23 @@
+let maxDiam = 50
+
 function setup() {
+    createCanvas(600, 600);
 }
 
+
 function draw() {
+    background("orange");
+    fill("pink");
+
+    for (let xpos = 0; xpos <= width; xpos += maxDiam) {
+       for (let ypos = 0; ypos <= height; ypos += maxDiam) {
+           for(let dim = maxDiam; dim >5; dim -= 8 ) {
+              ellipse (xpos, ypos, dim, dim);
+           }
+        }
+    }
 }
+
+   //ellipse(0, 0, eDiam, eDiam);
+   //ellipse(0, eDiam, eDiam, eDiam);
+   //ellipse(0, 2*eDiam,eDiam, eDiam);
